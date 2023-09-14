d322b5340dd75e6f1f0ce15b198dfdf54c6c3229
Jam Timbol <jam.timbol00@gmail.com>
2023-09-14 14:34:41 +0200

    Initiate Project

---
+++ src/test/java/com/learning/java/springlamiapizzeriacrud/SpringLaMiaPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.java.springlamiapizzeriacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
