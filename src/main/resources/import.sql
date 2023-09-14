6e0f0cf76c6150a89d25f8f4b631a09e52d8b708
Jam Timbol <jam.timbol00@gmail.com>
2023-09-14 17:42:12 +0200

    Done

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `pizza` (`price`, `description`, `img_url`, `name`) VALUES ('6.99', 'pizza ok', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/260px-Eq_it-na_pizza-margherita_sep2005_sml.jpg', 'Margherita');
+
+INSERT INTO `pizza` (`price`, `description`, `img_url`, `name`) VALUES ( '7.99', 'pizza okay', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/P_20230902_191843_1.jpg/1280px-P_20230902_191843_1.jpg', 'Capriciosa');
